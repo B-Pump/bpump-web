f96a22f8b6d29bec5ceefb29532e1ef66ed7e604
wiizz <wiizz76.contact@gmail.com>
2024-02-22 21:02:54 +0100

    feat: galaad à toi !

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -5,11 +5,49 @@ import { toast } from "sonner";
 import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
 
+import { Tooltip } from "@/components/ui/tooltip";
 import useFetch from "../api/bpump";
 
 export default function Home() {
     const { data, isLoading, error } = useFetch("GET", "exos/all");
-    console.log(data);
+    const people = [
+        {
+            id: 1,
+            name: "Pierre H.",
+            designation: "Programmation",
+            image: "https://black_hole-3kf-1-q4182424.deta.app/api/photo/zxafgkb7dy1w.png",
+        },
+        // {
+        //     id: 2,
+        //     name: "Auguste C.",
+        //     designation: "Programmation",
+        //     image: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+        // },
+        {
+            id: 3,
+            name: "Galaad G.",
+            designation: "Programmation",
+            image: "https://black_hole-3kf-1-q4182424.deta.app/api/photo/13p2skah8fqp.png",
+        },
+        // {
+        //     id: 4,
+        //     name: "Emma B.",
+        //     designation: "Design",
+        //     image: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+        // },
+        // {
+        //     id: 5,
+        //     name: "Nathan C.",
+        //     designation: "Programmation",
+        //     image: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80",
+        // },
+        // {
+        //     id: 6,
+        //     name: "Léo B.",
+        //     designation: "Communication",
+        //     image: "https://images.unsplash.com/photo-1544725176-7c40e5a71c5e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3534&q=80",
+        // },
+    ];
 
     return (
         <section className="grid items-center gap-6">
@@ -36,6 +74,9 @@ export default function Home() {
                 >
                     Notif
                 </Button>
+                <div className="flex flex-row items-center justify-center mb-10 w-full">
+                    <Tooltip items={people} />
+                </div>
             </div>
             <div>
                 {isLoading ? (
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "black_hole-3kf-1-q4182424.deta.app",
+            },
+        ],
+    },
+};
 
 export default nextConfig;
