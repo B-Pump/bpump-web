a71e75e7639e7afa72c20ac56de7f46f9e27dc65
wiizz <wiizz76.contact@gmail.com>
2024-02-22 21:12:26 +0100

    fix: users

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -17,36 +17,36 @@ export default function Home() {
             designation: "Programmation",
             image: "https://black_hole-3kf-1-q4182424.deta.app/api/photo/zxafgkb7dy1w.png",
         },
-        // {
-        //     id: 2,
-        //     name: "Auguste C.",
-        //     designation: "Programmation",
-        //     image: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
-        // },
+        {
+            id: 2,
+            name: "Auguste C.",
+            designation: "Programmation",
+            image: "https://black_hole-3kf-1-q4182424.deta.app/api/photo/nalmvys51hlp.jpeg",
+        },
         {
             id: 3,
             name: "Galaad G.",
             designation: "Programmation",
             image: "https://black_hole-3kf-1-q4182424.deta.app/api/photo/13p2skah8fqp.png",
         },
-        // {
-        //     id: 4,
-        //     name: "Emma B.",
-        //     designation: "Design",
-        //     image: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
-        // },
-        // {
-        //     id: 5,
-        //     name: "Nathan C.",
-        //     designation: "Programmation",
-        //     image: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80",
-        // },
-        // {
-        //     id: 6,
-        //     name: "Léo B.",
-        //     designation: "Communication",
-        //     image: "https://images.unsplash.com/photo-1544725176-7c40e5a71c5e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3534&q=80",
-        // },
+        {
+            id: 4,
+            name: "Emma B.",
+            designation: "Design",
+            image: "https://black_hole-3kf-1-q4182424.deta.app/api/photo/t2z09p1bzxmx.png",
+        },
+        {
+            id: 5,
+            name: "Nathan C.",
+            designation: "Programmation",
+            image: "https://black_hole-3kf-1-q4182424.deta.app/api/photo/tcnrpl9x6f99.png",
+        },
+        {
+            id: 6,
+            name: "Léo B.",
+            designation: "Communication",
+            image: "https://black_hole-3kf-1-q4182424.deta.app/api/photo/giv23ixqn90o.png",
+        },
     ];
 
     return (
