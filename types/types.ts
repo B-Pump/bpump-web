30333032549c8d4922a33fbf45dd30ec39855fc6
wiizz <wiizz76.contact@gmail.com>
2024-03-23 18:51:32 +0100

    feat: api data types

---
+++ types/types.ts
@@ -0,0 +1,21 @@
+interface Exos {
+    id: string;
+    title: string;
+    description: string;
+    category: string;
+    difficulty: number;
+    muscles: string[];
+    security: string[];
+    needed: string[];
+    calories: number;
+}
+
+interface Progs {
+    id: string;
+    title: string;
+    description: string;
+    category: string;
+    difficulty: number;
+    hint: string[];
+    exercises: string[];
+}
