06dd15666a0812c995c23f44d91e5b9d223a2a16
wiizzl <wiizz76.contact@gmail.com>
2024-05-02 00:25:23 +0200

    fix: navbar color

--- src/components/aceternity/navbar.tsx
+++ src/components/aceternity/navbar.tsx
@@ -39,7 +39,7 @@ export const FloatingNavBar = () => {
                     opacity: visible ? 1 : 0,
                 }}
                 transition={{ duration: 0.2 }}
-                className="fixed inset-x-0 top-7 z-50 mx-auto max-w-fit space-x-2 rounded-full bg-background px-4 py-2 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]"
+                className="fixed inset-x-0 top-7 z-50 mx-auto max-w-fit space-x-2 rounded-full bg-accent-foreground px-4 py-2 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]"
             >
                 {config.link.map((item, index: number) => (
                     <Link
@@ -47,7 +47,7 @@ export const FloatingNavBar = () => {
                         href={`/${item.href}`}
                         className={buttonVariants({ variant: "ghost", className: "hover:bg-transparent" })}
                     >
-                        <span className="text-sm">{item.title}</span>
+                        <span className="text-sm text-accent">{item.title}</span>
                     </Link>
                 ))}
             </motion.div>
