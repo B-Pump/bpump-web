a7b83ccb3ba62054844caca497a4174951e9ac4b
wiizzl <wiizz76.contact@gmail.com>
2024-05-12 18:31:22 +0200

    fix: new 3d model

--- src/components/model.tsx
+++ src/components/model.tsx
@@ -1,4 +1,4 @@
-import { ContactShadows, Environment, OrbitControls } from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 
@@ -6,18 +6,15 @@ import { Model } from "@/components/model/bpump";
 
 export function BPump() {
     return (
-        <Canvas shadows camera={{ position: [0, 0, 3], fov: 45 }}>
-            <ambientLight intensity={0.7} />
-            <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow />
-            <ContactShadows position={[0, -0.8, 0]} opacity={0.25} scale={10} blur={1.5} far={0.8} />
-            <Environment preset="city" />
+        <Canvas>
+            <ambientLight intensity={1} />
             <OrbitControls
                 minPolarAngle={Math.PI / 2}
                 maxPolarAngle={Math.PI / 2}
                 enableZoom={false}
                 enablePan={false}
                 autoRotate={true}
-                autoRotateSpeed={1}
+                autoRotateSpeed={2}
             />
             <Suspense fallback={null}>
                 <Model />
